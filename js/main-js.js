a7a4a4da2a8d59f694500e6e5757d680f5788109
Khrystyns Khozhai <l.xrustja@gmail.com>
2018-10-09 15:23:11 +0300

    added icons and image. created header

--- js/main-js.js
+++ js/main-js.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+  $('.language').click(function() {
+    $('.language > li ul').slideToggle();
+  });
+
+  if ($(window).width() > 1024) {
+    $('.menu-down').hover(function () {
+        $(this).find('.sub-menu').fadeIn(200);
+    }, function () {
+      $(this).find('.sub-menu').fadeOut(200);
+    });
+  }
+  if ($(window).width() <= 1024) {
+    $('.menu-down').click(function () {
+      $(this).find('.sub-menu').fadeToggle(200);
+    });
+  }
 
  
 
